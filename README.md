f006b898f2124070e1b626e6a9362c865fbb55ed
Keiran Smith <contact@keiran.scot>
2018-06-23 21:26:07 +0100

    Extend Handler to call OCR on Notification

--- README.md
+++ README.md
@@ -6,8 +6,12 @@ notifications and subscriptions.
 ## How do I set this up
 
 - Deploy the function with `faas-cli deploy -f openfaas-sns.yml`
+- Deploy the OCR function from the openfaas store
 - Setup an AWS Topic
 - Add an HTTP(s) Subscriber pointing to your function
+- [Setup an S3 Bucket to publish to the Topic](https://docs.aws.amazon.com/AmazonS3/latest/dev/ways-to-add-notification-config-to-bucket.html)
+- Upload an image with public access to the bucket
+- Watch the sns function logs
 
 ## Caveats
 
--- openfaas-sns/handler.go
+++ openfaas-sns/handler.go
@@ -1,10 +1,52 @@
 package function
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/robbiet480/go.sns"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"time"
 )
+
+type s3Notification struct {
+	Records []struct {
+		EventVersion string    `json:"eventVersion"`
+		EventSource  string    `json:"eventSource"`
+		AwsRegion    string    `json:"awsRegion"`
+		EventTime    time.Time `json:"eventTime"`
+		EventName    string    `json:"eventName"`
+		UserIdentity struct {
+			PrincipalID string `json:"principalId"`
+		} `json:"userIdentity"`
+		RequestParameters struct {
+			SourceIPAddress string `json:"sourceIPAddress"`
+		} `json:"requestParameters"`
+		ResponseElements struct {
+			XAmzRequestID string `json:"x-amz-request-id"`
+			XAmzID2       string `json:"x-amz-id-2"`
+		} `json:"responseElements"`
+		S3 struct {
+			S3SchemaVersion string `json:"s3SchemaVersion"`
+			ConfigurationID string `json:"configurationId"`
+			Bucket          struct {
+				Name          string `json:"name"`
+				OwnerIdentity struct {
+					PrincipalID string `json:"principalId"`
+				} `json:"ownerIdentity"`
+				Arn string `json:"arn"`
+			} `json:"bucket"`
+			Object struct {
+				Key       string `json:"key"`
+				Size      int    `json:"size"`
+				ETag      string `json:"eTag"`
+				Sequencer string `json:"sequencer"`
+			} `json:"object"`
+		} `json:"s3"`
+	} `json:"Records"`
+}
 
 // Handle a serverless request
 func Handle(req []byte) string {
@@ -36,5 +78,34 @@ func Handle(req []byte) string {
 }
 
 func HandleNotification(message string) string {
-	return message
+	var notification s3Notification
+	json.Unmarshal([]byte(message), &notification)
+	url := fmt.Sprintf("http://s3-%s.amazonaws.com/%s/%s",
+		notification.Records[0].AwsRegion,
+		notification.Records[0].S3.Bucket.Name,
+		notification.Records[0].S3.Object.Key)
+
+	return CallFunction("http://ocr:8080", url)
+}
+
+func CallFunction(function string, imageUrl string) string {
+	imageUrlBytes := []byte(imageUrl)
+	req, err := http.NewRequest("POST", function, bytes.NewBuffer(imageUrlBytes))
+	if err != nil {
+		return fmt.Sprintf("http.NewRequest() error: %v\n", err)
+	}
+
+	c := &http.Client{}
+	resp, _ := c.Do(req)
+	if err != nil {
+		return fmt.Sprintf("http.Do() error: %v\n", err)
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Sprintf("ioutil.ReadAll() error: %v\n", err)
+	}
+
+	return fmt.Sprintf("Parsed %s :\n%v\n", imageUrl, string(data))
 }
